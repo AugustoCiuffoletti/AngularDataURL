569acf7cde641ed9b9394df3fb9712845a9eff24
AugustoCiuffoletti <augusto@di.unipi.it>
2022-05-27 13:08:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-sgzqnl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-sgzqnl)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+/*
+Consult first https://en.wikipedia.org/wiki/Data_URI_scheme for the "data:"" URL scheme
+data:[<mediatype>][;base64],<data>. This alone allows 
+*/
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  downloadUrl: SafeUrl;
+  constructor(private sanitizer: DomSanitizer) {
+    const content = 'Hallo world!'; // Content to transfer
+    const mediatype = 'text/plain';
+    this.downloadUrl = this.sanitizer.bypassSecurityTrustUrl(
+      'data:' + mediatype + ',' + content
+    );
+    console.log(this.downloadUrl);
+  }
+}
