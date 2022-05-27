cce65199350fa7a37d4ba4b3baeaf9715f34badf
AugustoCiuffoletti <augusto@di.unipi.it>
2022-05-27 15:06:36 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 /*
-Consult first https://en.wikipedia.org/wiki/Data_URI_scheme for the "data:"" URL scheme. Ina word
-
-data:[<mediatype>][;base64],<data>
-This alone allows 
+Consult first https://en.wikipedia.org/wiki/Data_URI_scheme for the "data:"" URL scheme.
+In a word the URL scheme
+  data:[<mediatype>][;base64],<data>
+allows to transfer to the user data embedded in the HTML page. However, this is considered
+since a bad developer might inject dangerous data, like a script. Angular automatically
+prevents such kind of attack by inspecting the URl and blocking possibly dangerous ones. However, it
+gives the deleloper the possibility to avoid such ckeck. The method bypassSecurityTrustUrl is used
+to bybass the  controls performed by the "sanitizer", with the effect of allowing the processing of the data URL
 */
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
@@ -15,8 +19,9 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 export class AppComponent {
   downloadUrl: SafeUrl;
   constructor(private sanitizer: DomSanitizer) {
-    const content = 'Hallo world!'; // Content to transfer
-    const mediatype = 'text/plain'; // media type
+    var content = 'Hallo world!'; // Content to transfer
+    const mediatype = 'text/plain'; // media type (consider also '/text/json;base64')
+    //    content = encodeURIComponent(content); // Url encode content (needed for JSON)
     this.downloadUrl = this.sanitizer.bypassSecurityTrustUrl(
       'data:' + mediatype + ',' + content
     );
